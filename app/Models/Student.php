4f8528cdf8ff1fa4aa7c49347620eed04346a5c4
Vahid Molaei Bejarbaneh <91679087+vmolaei@users.noreply.github.com>
2021-10-14 05:00:09 +0330

    Start Laravel Project with Sanctum Authentication.

---
+++ app/Models/Student.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Laravel\Sanctum\HasApiTokens;
+
+class Student extends Model
+{
+    use HasFactory,HasApiTokens;
+    protected $table = "students";
+    protected $fillable = ["name","email","password","phone_no"];
+
+    public $timestamps = false;
+}
